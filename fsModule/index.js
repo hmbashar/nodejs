dbef14b2deb7842b006da2fbd601e4df3577e303
hmbashar <hmbashar@gmail.com>
2024-10-25 01:38:02 +0600

    fs.readFileSync

--- fsModule/index.js
+++ fsModule/index.js
@@ -7,12 +7,18 @@ let server = http.createServer(function(request, response) {
     if(request.url === '/') {
         
         //Async
-        fs.readFile('home.html', function(error, data) {
+        // fs.readFile('home.html', function(error, data) {
 
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
+        //     response.writeHead(200, {'Content-Type': 'text/html'});
+        //     response.write(data);
+        //     response.end();
+        // });
+
+        //Sync
+        let dataSync = fs.readFileSync('home.html');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(dataSync);
+        response.end();
     }
 
 });
